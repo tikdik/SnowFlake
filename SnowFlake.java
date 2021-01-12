beffe6e9c972e6aa17b394be0d2ac276ff475b43
tikdik <tikdik1988@gmail.com>
2021-01-12 18:05:13 +0800

    基于百度修改

--- SnowFlake.java
+++ SnowFlake.java
@@ -1,28 +1,31 @@
+import java.util.concurrent.TimeUnit;
 
 /**
  * twitter的snowflake算法 -- java实现
- * 
+ *
  * @author beyond
  * @date 2016/11/26
  */
 public class SnowFlake {
+    private static SnowFlake snowFlake = new SnowFlake(1);
 
     /**
-     * 起始的时间戳
+     * 起始的时间戳 2021-01-12 17:28:49
      */
-    private final static long START_STMP = 1480166465631L;
+    private final static long START_TIMESTAMP = 1610443729000L;
 
     /**
-     * 每一部分占用的位数
+     * 序列号占用的位数
      */
-    private final static long SEQUENCE_BIT = 12; //序列号占用的位数
-    private final static long MACHINE_BIT = 5;   //机器标识占用的位数
-    private final static long DATACENTER_BIT = 5;//数据中心占用的位数
+    private final static long SEQUENCE_BIT = 13;
+    /**
+     * 机器标识占用的位数
+     */
+    private final static long MACHINE_BIT = 10;
 
     /**
      * 每一部分的最大值
      */
-    private final static long MAX_DATACENTER_NUM = -1L ^ (-1L << DATACENTER_BIT);
     private final static long MAX_MACHINE_NUM = -1L ^ (-1L << MACHINE_BIT);
     private final static long MAX_SEQUENCE = -1L ^ (-1L << SEQUENCE_BIT);
 
@@ -30,23 +33,32 @@ public class SnowFlake {
      * 每一部分向左的位移
      */
     private final static long MACHINE_LEFT = SEQUENCE_BIT;
-    private final static long DATACENTER_LEFT = SEQUENCE_BIT + MACHINE_BIT;
-    private final static long TIMESTMP_LEFT = DATACENTER_LEFT + DATACENTER_BIT;
+    private final static long TIMESTAMP_LEFT = SEQUENCE_BIT + MACHINE_BIT;
+    /**
+     * 机器标识
+     */
+    private long machineId;
+    /**
+     * 序列号
+     */
+    private long sequence = 0L;
+    /**
+     * 上一次时间戳
+     */
+    private long lastTimestamp = -1L;
 
-    private long datacenterId;  //数据中心
-    private long machineId;     //机器标识
-    private long sequence = 0L; //序列号
-    private long lastStmp = -1L;//上一次时间戳
-
-    public SnowFlake(long datacenterId, long machineId) {
-        if (datacenterId > MAX_DATACENTER_NUM || datacenterId < 0) {
-            throw new IllegalArgumentException("datacenterId can't be greater than MAX_DATACENTER_NUM or less than 0");
-        }
+    protected SnowFlake(long machineId) {
         if (machineId > MAX_MACHINE_NUM || machineId < 0) {
             throw new IllegalArgumentException("machineId can't be greater than MAX_MACHINE_NUM or less than 0");
         }
-        this.datacenterId = datacenterId;
         this.machineId = machineId;
+    }
+
+    /**
+     * 获取16进制字符串
+     */
+    public static String UUID() {
+        return Long.toHexString(snowFlake.nextId());
     }
 
     /**
@@ -55,49 +67,47 @@ public class SnowFlake {
      * @return
      */
     public synchronized long nextId() {
-        long currStmp = getNewstmp();
-        if (currStmp < lastStmp) {
+        long currentSecond = getCurrentSecond();
+        if (currentSecond < lastTimestamp) {
             throw new RuntimeException("Clock moved backwards.  Refusing to generate id");
         }
 
-        if (currStmp == lastStmp) {
+        if (currentSecond == lastTimestamp) {
             //相同毫秒内，序列号自增
             sequence = (sequence + 1) & MAX_SEQUENCE;
             //同一毫秒的序列数已经达到最大
             if (sequence == 0L) {
-                currStmp = getNextMill();
+                currentSecond = getNextSecond(lastTimestamp);
             }
         } else {
             //不同毫秒内，序列号置为0
             sequence = 0L;
         }
 
-        lastStmp = currStmp;
-
-        return (currStmp - START_STMP) << TIMESTMP_LEFT //时间戳部分
-                | datacenterId << DATACENTER_LEFT       //数据中心部分
-                | machineId << MACHINE_LEFT             //机器标识部分
-                | sequence;                             //序列号部分
+        lastTimestamp = currentSecond;
+        //时间戳部分 | 机器标识部分 | 序列号部分
+        return (currentSecond - START_TIMESTAMP) << TIMESTAMP_LEFT
+                | machineId << MACHINE_LEFT
+                | sequence;
     }
 
-    private long getNextMill() {
-        long mill = getNewstmp();
-        while (mill <= lastStmp) {
-            mill = getNewstmp();
-        }
-        return mill;
-    }
-
-    private long getNewstmp() {
-        return System.currentTimeMillis();
-    }
-
-    public static void main(String[] args) {
-        SnowFlake snowFlake = new SnowFlake(2, 3);
-
-        for (int i = 0; i < (1 << 12); i++) {
-            System.out.println(snowFlake.nextId());
+    /**
+     * Get next millisecond
+     */
+    private long getNextSecond(long lastTimestamp) {
+        long timestamp = getCurrentSecond();
+        while (timestamp <= lastTimestamp) {
+            timestamp = getCurrentSecond();
         }
 
+        return timestamp;
+    }
+
+    /**
+     * Get current second，单位还是毫秒
+     */
+    private long getCurrentSecond() {
+        long currentSecond = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis());
+        return currentSecond;
     }
 }
